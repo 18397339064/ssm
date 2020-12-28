732cab0fc56762cb2e3898992a6c30f3d9ea17b6
龙の傲天 <774002892@qq.com>
2020-12-28 09:43:22 +0800

    ty 分类 商品

--- ssm-dao/src/main/java/com/hnguigu/dao/StockDao.java
+++ ssm-dao/src/main/java/com/hnguigu/dao/StockDao.java
@@ -24,7 +24,7 @@ public interface StockDao {
     public int updateCountJia(Stock stock);
 
     //查询该商品在库存中是否存在
-    public Stock queryCom(int comid);
+    public Stock queryCom(@Param("comid") int comid,@Param("whid") int whid);
 
     //判断是否有某个商品在库存中有，如果有不能删除
     public int queryStockComid(int comid);
--- ssm-service/src/main/java/com/hnguigu/service/impl/StockServiceImpl.java
+++ ssm-service/src/main/java/com/hnguigu/service/impl/StockServiceImpl.java
@@ -53,8 +53,8 @@ public class StockServiceImpl implements StockService {
 
     //查询该商品在库存中是否存在
     @Override
-    public Stock queryCom(int comid) {
-        return stockDao.queryCom(comid);
+    public Stock queryCom(int comid,int whid) {
+        return stockDao.queryCom(comid,whid);
     }
 
     //判断是否有某个商品在库存中有，如果有不能删除
